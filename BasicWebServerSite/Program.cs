3a22bcdc4bac301c3a98f568861897bae7e6f5ed
Eduardo Spaki <eduardo@code21.com.br>
2020-12-04 10:54:16 -0300

    first commit

---
+++ BasicWebServerSite/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace BasicWebServerSite
+{
+    class Program
+    {
+        static void Main(string[] args) => WebHost
+            .CreateDefaultBuilder()
+            .Configure(app =>
+            {
+                app.Run(context => context.Response.WriteAsync($"<h1> Welcome to {DateTime.Now.Year} :) </h1>"));
+            })
+            .Build()
+            .Run();
+    }
+}
